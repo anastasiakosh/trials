eb66ff9328a446d83d958cc66a6a67ed8061f0e0
Vladislav <truitc00@gmail.com>
2025-08-06 11:38:47 +0300

    job with goto

---
+++ metk.pas
@@ -0,0 +1,16 @@
+program metki;
+
+
+label 
+	point;
+begin
+	writeln('we are before to the point.');
+	goto point;
+	point:
+	begin
+		writeln('lolka. this is my label');
+		writeln('lolka. this is my label');
+		writeln('lolka. this is my label');
+	end;
+end.
+		

8ce2809dc4683c0f758bb8e43dedd9f9c6f25352
Vladislav <truitc00@gmail.com>
2025-08-20 12:01:41 +0300

    преобразование типа с плавающей точкой в инты

--- arrays.pas
+++
@@ -1,4 +0,0 @@
-program matrix;
-type
-    matrix5x5 = array [1..5, 1..5] of real;
-    {or array [1..5] of array [1..7] of real}
---
+++ preobrazovanie_tipov.pas
@@ -0,0 +1,7 @@
+program rea;
+var
+    r: real = 15.75;
+begin
+    writeln(trunc(r));
+    writeln(round(r));
+end.

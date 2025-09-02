2821940cf5799f97749dec24b7270ffbb6b8712b
Vladislav <truitc00@gmail.com>
2025-09-02 21:31:08 +0300

    complete task 2.32

---
+++ 32.pas
@@ -0,0 +1,105 @@
+program ThirtyTwo;
+uses crt;
+
+const
+    Size = 5;
+    KeyLeft = -75;
+    KeyRight = -77;
+    KeyDown = -80;
+    KeyUp = -72;
+    KeyEscape = 27;
+    ColorCount = 16;
+    BGColCount = 8;
+
+type
+    Rectangle = record
+        BeginX, UpY, Key, CountOfBG, CountOfTX: integer;
+    end;
+
+procedure GetKey(var key: integer);
+var
+    c: char;
+begin
+    c := ReadKey;
+    if c = #0 then
+        key := -ord(ReadKey)
+    else
+        key := ord(c);
+end;
+
+procedure Print(var r: Rectangle);
+var
+    x, y: integer;
+begin
+    for y := r.UpY downto r.UpY - Size + 1 do
+        for x := r.BeginX to r.BeginX + Size - 1 do
+        begin
+            GotoXY(x, y);
+            write('*');
+        end;
+    GotoXY(1, 1);
+end;
+var 
+    SaveTextAttr: integer;
+   AllColors: array [1..ColorCount] of word =
+    (
+        Black, Blue, Green, Cyan,
+        Red, Magenta, Brown, LightGray,
+        DarkGray, LightBlue, LightGreen, LightCyan,
+        LightRed, LightMagenta, Yellow, White
+    ); 
+    r: Rectangle;
+begin
+    clrscr;
+    SaveTextAttr := TextAttr;
+    r.BeginX := (ScreenWidth div 2) - Size;
+    r.UpY := (ScreenHeight div 2) + Size;
+    r.CountOfBG := 1;
+    r.CountOfTX := 1;
+    Print(r);
+    while (r.Key <> KeyEscape) and (r.Key <> ord(' ')) do
+    begin
+        GetKey(r.Key);
+        if (r.Key = KeyDown) or (r.Key = KeyUp) or 
+            (r.Key = KeyRight) or (r.Key = KeyLeft) then
+        begin
+            case r.Key of 
+            KeyLeft:
+            begin
+                if (r.CountOfBG = 1) then
+                    r.CountOfBG := BGColCount
+                else
+                    r.CountOfBG := r.CountOfBG - 1;
+                TextBackground(AllColors[r.CountOfBG]);
+            end;
+            KeyRight:
+            begin
+                if (r.CountOfBG = BGColCount) then
+                    r.CountOfBG := 1
+                else
+                    r.CountOfBG := r.CountOfBG + 1;
+                TextBackground(AllColors[r.CountOfBG]);
+            end;
+            KeyDown:
+            begin
+                if (r.CountOfTX = 1) then
+                    r.CountOfTX := ColorCount 
+                else
+                    r.CountOfTX := r.CountOfTX - 1;
+                TextColor(AllColors[r.CountOfTX]);
+            end;
+            KeyUp:
+            begin
+                if (r.CountOfTX = ColorCount) then
+                    r.CountOfTX := 1
+                else
+                    r.CountOfTX := r.CountOfTX + 1;
+                TextColor(AllColors[r.CountOfTX]);
+            end;
+            end;
+            Print(r);
+        end;
+    end;
+    TextAttr := SaveTextAttr;
+    clrscr;
+end.
